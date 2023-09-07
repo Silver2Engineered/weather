d5ee730563e6a72577c7c483a60bc1c39e801a05
Ariella Silver <a.silver@u.northwestern.edu>
2023-09-07 14:57:06 -0400

    Create Project and Initial Layout

---
+++ app/src/main/java/com/example/weather/CityPicker.kt
@@ -0,0 +1,31 @@
+package com.example.weather
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CityPicker : Fragment() {
+
+    companion object {
+        fun newInstance() = CityPicker()
+    }
+
+    private lateinit var viewModel: CityPickerViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_city_picker, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CityPickerViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/weather/CityPickerViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weather
+
+import androidx.lifecycle.ViewModel
+
+class CityPickerViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
