0dbdb4ce107fa40c7dfad3b2af1e1222d14c86e2
Ariella Silver <a.silver@u.northwestern.edu>
2023-09-19 15:34:54 -0500

    making vcals const

--- app/src/main/java/com/example/weather/CityPickerViewModel.kt
+++ app/src/main/java/com/example/weather/CityPickerViewModel.kt
@@ -10,14 +10,15 @@ import com.example.weather.network.WeatherApi
 import com.example.weather.network.WeatherApiResponse
 import kotlinx.coroutines.launch
 
+const val cityIds = "4772354,5368361,5128581,4887398,4699066,5809844,5856195,5419384,5391959,4684888,5746545,4990729,4335045"
+const val appId = "bd4472d97ca0b479dc32513cf50a1bf3"
+const val units = "metric"
+
 class CityPickerViewModel : ViewModel() {
 
     private var _cities = MutableLiveData<List<City>>()
 
     val cities: LiveData<List<City>> = _cities
-    private val cityIds = "4772354,5368361,5128581,4887398,4699066,5809844,5856195,5419384,5391959,4684888,5746545,4990729,4335045"
-    private val appId = "bd4472d97ca0b479dc32513cf50a1bf3"
-    private val units = "metric"
 
     init {
         getCityInfo()
