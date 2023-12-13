6f0674af4c5500a133277ed1f338b545774eab77
Ariella Silver <a.silver@u.northwestern.edu>
2023-12-13 15:47:26 -0500

    citypickerviewmodel tests complete

--- app/src/test/java/com/example/weather/CityPickerViewModelTest.kt
+++ app/src/test/java/com/example/weather/CityPickerViewModelTest.kt
@@ -1,40 +1,47 @@
 package com.example.weather
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.example.weather.network.CityRoomDatabase
+import com.example.weather.CityPickerViewModel.Companion.convertCelsiusToFahrenheit
+import com.example.weather.CityPickerViewModel.Companion.formatLowAndHigh
 import com.example.weather.network.StateOverview
 import com.example.weather.repository.CityRepository
+import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
 import junit.framework.TestCase
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.setMain
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class CityPickerViewModelTest {
 
-    private lateinit var repository: CityRepository
-    private val database = mockk<CityRoomDatabase>(relaxed=true)
+    private val repository: CityRepository = mockk(relaxed = true)
     private lateinit var cityPickerViewModel: CityPickerViewModel
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
-
-    @Before
-    fun setup() {
-        repository = CityRepository(database)
+    @Test
+    fun `Get city overview works successfully`() = runBlocking {
+        Dispatchers.setMain(Dispatchers.Unconfined)
+        coEvery { repository.refreshCityOverview()} answers {
+            every { repository.cityOverview.value } returns
+                    StateOverview.Success(listOf(testCityOverview))
+        }
+        cityPickerViewModel = CityPickerViewModel.Factory(repository).create(CityPickerViewModel::class.java)
+        cityPickerViewModel.getCityInfo()
+        assert(cityPickerViewModel.cities.value is StateOverview.Success)
     }
 
     @Test
-    fun `Get city overview works successfully`() = runTest {
-        Dispatchers.setMain(Dispatchers.Unconfined)
-        val cityPickerViewModel = CityPickerViewModel(repository)
-        database.cityOverviewDao.insert(listOf(testCachedCityOverview))
-        cityPickerViewModel.getCityInfo()
-        Thread.sleep(5000)
-        TestCase.assertEquals(cityPickerViewModel.cities.value, StateOverview.Success(database.cityOverviewDao.getCities().map { it.toDomainModel() }))
+    fun `Temperature conversion works correctly`() {
+        TestCase.assertEquals(convertCelsiusToFahrenheit(27.0), 80)
+    }
+
+    @Test
+    fun `Formatter works correctly`() {
+        TestCase.assertEquals(formatLowAndHigh(testCityOverview), "932℉/932℉")
     }
 }
--- app/src/test/java/com/example/weather/DetailsViewModelTest.kt
+++ app/src/test/java/com/example/weather/DetailsViewModelTest.kt
@@ -1,49 +1,37 @@
 package com.example.weather
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
 import com.example.weather.network.CityRoomDatabase
 import com.example.weather.network.StateDetails
 import com.example.weather.repository.CityRepository
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
-import junit.framework.TestCase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.setMain
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class DetailsViewModelTest {
 
     private val repository: CityRepository = mockk(relaxed = true)
-    private val database = mockk<CityRoomDatabase>(relaxed=true)
     private lateinit var detailsViewModel: DetailsViewModel
 
 
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
-
-    @Before
-    fun setup() {
-        detailsViewModel = DetailsViewModel.Factory(repository).create(DetailsViewModel::class.java)
-    }
-
     @Test
     fun `Get city weather works successfully`() = runBlocking {
         Dispatchers.setMain(Dispatchers.Unconfined)
-        coEvery { repository.refreshCityDetails(any())} returns mockk {
-            coEvery { repository.cityDetails } returns MutableLiveData(
-                StateDetails.Success(
-                    testCityDetails
-                )
-            )
+        coEvery { repository.refreshCityDetails(any())} answers {
+            every { repository.cityDetails.value } returns
+                StateDetails.Success(testCityDetails)
         }
-        detailsViewModel.getCityWeather("500")
-        Thread.sleep(5000)
-        TestCase.assertEquals(detailsViewModel.cityData.value, StateDetails.Success(testCityDetails))
+        detailsViewModel = DetailsViewModel.Factory(repository).create(DetailsViewModel::class.java)
+        detailsViewModel.getCityWeather("")
+        assert(detailsViewModel.cityData.value is StateDetails.Success)
     }
 
     }
